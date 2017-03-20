986bc78f78cd4911279f372df9c4e475deccc7f5
Paróczi Gergő <paroczi@gmail.com>
2017-03-20 09:17:14 -0700

    tests for alarm

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,9 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.system.TrainSystem;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,13 +11,42 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensor sensor;
+    TrainController mockTC;
+    TrainUser mockUser;
+
     @Before
     public void before() {
-        // TODO Add initializations
+
+        mockTC = mock(TrainController.class);
+        mockUser = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(mockTC,mockUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void alarm1() {
+       sensor.overrideSpeedLimit(600);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+    @Test
+    public void alarm2() {
+        sensor.overrideSpeedLimit(-20);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void alarm3() {
+        when(mockTC.getReferenceSpeed()).thenReturn(100);
+        sensor.overrideSpeedLimit(1);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void alarm4() {
+        when(mockTC.getReferenceSpeed()).thenReturn(200);
+        sensor.overrideSpeedLimit(180);
+
+        verify(mockUser, times(0)).setAlarmState(true);
+
     }
 }
